85d7c6dd816fa293a6cb6a6a4c55274992447d65
Jonas G <jp08gim@gmail.com>
2025-03-25 14:04:53 +0100

    final

--- Movie_Collection-2/src/App.vue
+++ Movie_Collection-2/src/App.vue
@@ -17,6 +17,12 @@ const movieFunctions = movieStore
   <h1>Create your own Movie Collection</h1>
   <MovieForm v-if="oneMovie" />
   <button @click="oneMovie = {}" v-if="!oneMovie">Add movie to your list</button>
+  <button class="intitialAppButton submit" @click="oneMovie = {}" v-if="!allMovies.length && !oneMovie">
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
+      class="size-10">
+      <path stroke-linecap="round" stroke-linejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
+    </svg>
+  </button>
   <div class="wide" v-for="movie in allMovies" :key="movie.id" @click="oneMovie = movie">
     <MovieItem :movie="movie" />
   </div>
@@ -36,5 +42,33 @@ ul {
 
 .wide {
   width: 100vw;
+}
+
+.intitialAppButton {
+  margin-top: 20vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 10rem;
+  width: 20rem;
+  height: 20rem;
+  animation-name: pulse;
+  animation-duration: 2s;
+  animation-iteration-count: infinite;
+  border-radius: 100%;
+}
+
+@keyframes pulse {
+  0% {
+    opacity: 10%;
+  }
+
+  50% {
+    opacity: 100%;
+  }
+
+  100% {
+    opacity: 10%;
+  }
 }
 </style>
